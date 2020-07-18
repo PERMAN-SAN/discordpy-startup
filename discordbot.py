1eb6404a2f51f589a57ea3e42ab2ca295acfc303
PERMAN-SAN <68472223+PERMAN-SAN@users.noreply.github.com>
2020-07-19 01:53:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,16 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os
+import traceback
+
+token = os.environ['DISCORD_BOT_TOKEN']
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 client = commands.Bot(command_prefix='.')
 @client.event
